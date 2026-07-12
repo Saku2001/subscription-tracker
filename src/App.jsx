c132ea9748741381e66fd822cb1400f64e05eee9
Saku2001 <sajetku@gmail.com>
2026-07-12 22:04:45 +0100

    Layout component and bg changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Layout from "./components/layout/Layout";
+
 import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        {/* Main application pages */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<Dashboard />} />
 
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+
+        {/* Authentication pages */}
         <Route path="/login" element={<Login />} />
 
         <Route path="/register" element={<Register />} />
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,3 +1,9 @@
-export default function Layout({ children }) {
-  return <div>{children}</div>;
+import { Outlet } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <div className="min-h-screen bg-indigo-950">
+      <Outlet />
+    </div>
+  );
 }
