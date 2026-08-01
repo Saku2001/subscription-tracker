f2007ae5e52be51dbab77913e82574f2e940c108
Saku2001 <sajetku@gmail.com>
2026-08-01 21:42:58 +0100

    Progress Timeline component (adding dates)

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -5,15 +5,45 @@ export default function Timeline() {
 
   const days = [];
 
-  for (let i = -15; i <= 15; i++) {
+  for (let i = -10; i <= 10; i++) {
     days.push(addDays(today, i));
   }
 
+  const todayString = format(today, "yyyy-MM-dd");
   console.log(days);
 
   return (
-    <div>
+    <div className="mt-10">
       <h1>Timeline</h1>
+      <div className="flex gap-10 overflow-x-auto whitespace-nowrap">
+        {" "}
+        {days.map((day) => {
+          const dayString = format(day, "yyyy-MM-dd");
+          const isToday = dayString === todayString;
+          return (
+            <div
+              key={day.toISOString()}
+              className="flex flex-col items-center min-w-[40px]"
+            >
+              <span
+                className={`${
+                  isToday
+                    ? "text-blue-600 font-bold text-lg"
+                    : "text-gray-700 text-sm"
+                }`}
+              >
+                {format(day, "d")}
+              </span>
+
+              <div
+                className={`mt-2 ${
+                  isToday ? "w-1 h-10 bg-blue-600" : "w-[2px] h-6 bg-gray-400"
+                }`}
+              ></div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
